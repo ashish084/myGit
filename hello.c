deca9997fe5c7bc310d20fb3602a3ec34c72bd26
Ramakant Oja <roja@my.okcu.edu>
2015-03-12 20:20:40 -0500

    this is been modified

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ main()
 printf("This is my first git program\n");
 printf("This is a second modification to hello word to test git \n");
 printf("This is new modification \n");
+printf("this file is been edited \n");
 =======
 { 
 	printf("this is first use of git \n");
